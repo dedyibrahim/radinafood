7799a36eb50401db750ef2a96a8dacf5d02926b9
Dedy ibrahim <dedyibrahym23@gmail.com>
2024-09-21 13:27:30 +0700

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,18 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   runtimeConfig: {
     apiSecret: '123',
-
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || '/api'
     }
   },
   
   app: {
-   // pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: "RadinaFood - No Food No Yummy",
       link: [
         {
           rel: "stylesheet",
-          href:
-            "https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap",
         },
       ],
       script: [
@@ -29,37 +26,33 @@ export default defineNuxtConfig({
   },
 
   devtools: { enabled: true },
+  
   plugins: [
-      { src: "~/plugins/apexCharts.js", mode: "client" },
-      { src: "~/plugins/animate.js", mode: "client" },
-      { src: "~/plugins/worldCloud.js", mode: "client" }, 
-      { src: "~/plugins/masonry.js", mode: "client" }, 
-      { src: "~/plugins/vuegraph.js", mode: "client" }, 
-    
+    { src: "~/plugins/apexCharts.js", mode: "client" },
+    { src: "~/plugins/animate.js", mode: "client" },
+    { src: "~/plugins/worldCloud.js", mode: "client" }, 
+    { src: "~/plugins/masonry.js", mode: "client" }, 
+    { src: "~/plugins/vuegraph.js", mode: "client" },
   ],
+  
   css: ['vuetify/lib/styles/main.sass', '~/assets/main.css'],
+  
   build: {
     transpile: ['vuetify'],
   },
-  modules: ['@pinia/nuxt', '@vite-pwa/nuxt','@pinia-plugin-persistedstate/nuxt',
-  [
-    "@pinia/nuxt",
-    {
-      autoImports: ["defineStore", "acceptHMRUpdate"],
-    },
-  ],'nuxt-icon',
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
-    },
+  
+  modules: [
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    'nuxt-icon'
   ],
+  
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
       name: 'RadinaFood - No Food No Yummy',
-      short_name: 'RadinaFood - No Food No Yummy',
+      short_name: 'RadinaFood',
       theme_color: '#03045Ef',
       icons: [
         {
@@ -82,8 +75,6 @@ export default defineNuxtConfig({
     },
     client: {
       installPrompt: true,
-      // you don't need to include this: only for testing purposes
-      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
       periodicSyncForUpdates: 20,
     },
     devOptions: {
@@ -93,16 +84,20 @@ export default defineNuxtConfig({
       type: 'module',
     },
   },
+  
   vite: {
     server: {
       fs: {
-          allow: [".."]
+        allow: [".."]
       }
-  },
+    },
     vue: {
       template: {
         transformAssetUrls,
       },
     },
+    plugins: [
+      vuetify({ autoImport: true })
+    ],
   },
 })
